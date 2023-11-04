c1a067289d91822345fbdca3585f2adba82ec923
shreyasa31 <shreyasa@usc.edu>
2023-11-04 13:56:23 -0700

    Update Items.js

--- ASSIGNMENT_3/frontendfinal/my-app/src/Component/Items.js
+++ ASSIGNMENT_3/frontendfinal/my-app/src/Component/Items.js
@@ -42,7 +42,7 @@ const ItemsTable = ({items,handleBack}) => {
   return (
     <>
     {/* */}
-    <div className="container mt-5">
+    {/* <div className="container mt-5">
     <h2 onClick={traverseCallback }> List</h2>
       {(
        <table>
@@ -77,11 +77,11 @@ const ItemsTable = ({items,handleBack}) => {
             )}
             {items?.itemSpecifics && items?.itemSpecifics.length > 0 && (
         <tr>
-          <td>Item Specifics</td>
-          <td>
+          {/* <td>Item Specifics</td> */}
+          {/* <td>
             {items.itemSpecifics.map((specific, index) => (
               <div key={index}>
-                <strong>{specific.name}:</strong> {specific.value}
+                <strong>{specific.name}</strong> {specific.value}
               </div>
             ))}
           </td>
@@ -93,8 +93,82 @@ const ItemsTable = ({items,handleBack}) => {
       
     </div>
     </>
-  );
-                  }
+  ); */} 
+  <div className="container mt-5">
+  <h2 onClick={traverseCallback}>List</h2>
+  <div className="table-responsive">
+    <table className="table table-dark table-striped">
+      <tbody>
+        {items?.photo && (
+          <tr>
+            <td>Photo</td>
+            <td>
+              {items?.photo.map((url, index) => (
+                <img key={index} src={url} alt="Item" width="50" />
+              ))}
+            </td>
+          </tr>
+        )}
+        {items?.price && (
+          <tr>
+            <td>Price</td>
+            <td>{items.price}</td>
+          </tr>
+        )}
+        {items?.location && (
+          <tr>
+            <td>Location</td>
+            <td>{items.location}</td>
+          </tr>
+        )}
+        {items?.returnPolicy && (
+          <tr>
+            <td>Return Policy</td>
+            <td>
+              {items?.returnPolicy?.returnsAccepted
+                ? `Returns Accepted within ${items?.returnPolicy?.returnsWithin}`
+                : "Returns Not Accepted"}
+            </td>
+          </tr>
+        )}
+        {/* {items?.itemSpecifics && items?.itemSpecifics.length > 0 && (
+          <tr>
+            {/* <td>Item Specifics</td> */}
+           
+             {/* <td>
+
+
+             </td>
+             
+                  {items.itemSpecifics.map((specific, index) => (
+                    <tr key={index}>
+                     <td><strong>{specific.name}</strong></td> 
+                    <td> {specific.value}</td>
+                    </tr>
+                  ))}
+               
+             
+            
+          </tr>
+        )} */}
+        {items?.itemSpecifics && items?.itemSpecifics.length > 0 && (
+              <tr>
+                <td>Name</td>
+                <td>Value</td>
+              </tr>
+            )}
+            {items.itemSpecifics && items.itemSpecifics.map((specific, index) => (
+              <tr key={index}>
+                <td><strong>{specific.name}</strong></td>
+                <td>{specific.value}</td>
+              </tr>
+            ))}
+      </tbody>
+    </table>
+  </div>
+</div>
+</>
+);                  }
 
 
 
