3fe2d0a1280e593198dcb817e3722ca3bf037979
shreyasa31 <shreyasa@usc.edu>
2023-12-07 04:34:48 -0800

    items page wishlistdisplay prgress bar and notext

--- app/src/main/java/com/example/ebayshoppingapplictaion/WishlistFragment.java
+++ app/src/main/java/com/example/ebayshoppingapplictaion/WishlistFragment.java
@@ -5,6 +5,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -26,6 +29,9 @@ import java.util.List;
 public class WishlistFragment extends Fragment implements WishlistAdapter.OnItemClickListener {
 
     private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private TextView noResultsTextView;
+    private TextView searchProductsText;
     private WishlistAdapter adapter;
     private List<WishlistItem> wishlistItems = new ArrayList<>();
 
@@ -33,6 +39,12 @@ public class WishlistFragment extends Fragment implements WishlistAdapter.OnItem
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_wishlist, container, false);
         recyclerView = view.findViewById(R.id.recyclerView);
+        progressBar = view.findViewById(R.id.progressBar);
+        searchProductsText = view.findViewById(R.id.searchProductsText);
+        noResultsTextView=view.findViewById(R.id.noResultsTextView);
+        // Initially, show the progress bar and hide the text view
+        progressBar.setVisibility(View.VISIBLE);
+        searchProductsText.setVisibility(View.VISIBLE);
         setupRecyclerView(); // Initialize the RecyclerView and adapter here
         fetchWishlistItems(); // Then fetch the wishlist items
         return view;
@@ -41,11 +53,13 @@ public class WishlistFragment extends Fragment implements WishlistAdapter.OnItem
     private void fetchWishlistItems() {
         String url = "http://10.0.2.2:8080/getWishlist";
         Log.d("wishlistgetapi", "apiiiiiiiii" + url);
-
+        progressBar.setVisibility(View.VISIBLE); // Show the progress bar when fetching begins
+        searchProductsText.setVisibility(View.VISIBLE);
         StringRequest request = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+
                         Log.d("APIResponseeeeeeee", "Response: " + response);
                         try {
                             JSONArray jsonArray = new JSONArray(response);
@@ -66,10 +80,23 @@ public class WishlistFragment extends Fragment implements WishlistAdapter.OnItem
                                 wishlistItems.add(newItem);
                                 Log.d("responseeeee","thijgdw"+wishlistItems);
                             }
-                            adapter.notifyDataSetChanged(); // Notify the adapter
+//                            recyclerView.setVisibility(View.VISIBLE);
+//                            adapter.notifyDataSetChanged(); // Notify the adapter
+                            if (wishlistItems.isEmpty()) {
+                                noResultsTextView.setVisibility(View.VISIBLE); // Show no results text
+                                recyclerView.setVisibility(View.GONE); // Hide RecyclerView
+                            } else {
+                                recyclerView.setVisibility(View.VISIBLE); // Show RecyclerView
+                                noResultsTextView.setVisibility(View.GONE); // Hide no results text
+                                adapter.notifyDataSetChanged(); // Notify the adapter
+                            }
                         } catch (Exception e) {
                             e.printStackTrace();
                             // Handle the error
+                        }
+                        finally {
+                            progressBar.setVisibility(View.GONE); // Hide the progress bar when fetching is complete
+                            searchProductsText.setVisibility(View.GONE);
                         }
                     }
                 }, new Response.ErrorListener() {
